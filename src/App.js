9478e8040fac27f78405e10250514df87a4a8236
johanna <jd.34736.jdlt@gmail.com>
2021-09-12 20:09:11 -0400

    destructured the count, added another destructured count called 'count2'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, {useState} from "react";
 
 const App = () => {
-  const [count, setCount] = useState(10);
+  const [{count, count2 }, setCount] = useState({count: 10, count2: 20 });
 
   return (
   <div>
     <button onClick={() => setCount(currentCount => currentCount + 1)}>
       +
       </button>
+      <div>
+      count: 1 {count}
+    </div>
     <div>
-      {count}
+      count 2: {count2}
     </div>
   </div>
   );
