7bda85048789210a5d8601446757daee279404e1
johanna <jd.34736.jdlt@gmail.com>
2021-09-12 23:35:30 -0400

    learned and implemented a 'cleanup' function to useEffect hook

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { useEffect } from "react";
 import { useForm } from "./useForm";
 
 const App = () => {
-	const [values, handleChange] = useForm({ email: "", password: "" });
+	const [values, handleChange] = useForm({
+		email: "",
+		password: "",
+		firstName: "",
+	});
 
 	useEffect(() => {
 		console.log("render");
-	}, [values.password]);
+
+		//'cleanup' function
+		return () => {
+			console.log("unmount");
+		};
+	}, []);
 
 	return (
 		<div>
 			<input name="email" value={values.email} onChange={handleChange} />
 			<input
+				name="firstName"
+				value={values.firstName}
+				placeholder="first name"
+				onChange={handleChange}
+			/>
+			<input
 				type="password"
 				value={values.password}
+				placeholder="password"
 				name="password"
 				onChange={handleChange}
 			/>
